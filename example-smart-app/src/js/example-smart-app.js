b13d5ec93d027e99c5ad91a93129d8d41cbc07f6
abdulki <abdul.kazi@nhs.net>
2021-06-03 14:59:30 +0100

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -31,12 +31,12 @@
                       }
 					});
 
-        $.when(pt, obv).fail(onError);
+        $.when(pt, obv,alg).fail(onError);
 
-        $.when(pt, obv).done(function(patient, obv) {
+        $.when(pt, obv,alg).done(function(patient, obv,allergies) {
 			console.log(patient);
 			console.log(obv);
-			//console.log(allergies);
+			console.log(allergies);
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
 
