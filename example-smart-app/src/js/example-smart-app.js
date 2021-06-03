a5e3c9482f47ee2ff8844eff4b83f88b981e802d
abdulki <abdul.kazi@nhs.net>
2021-06-03 16:06:33 +0100

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -53,6 +53,15 @@
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
+		  var allergyTable="<table>";
+		  var allergyLen=allergies.length
+		  for (var i=0, len=allergyLen;i++){
+			  allergyTable+="<tr><td>"+allergies[i].substance.text+"</td></tr>";
+			  if (allergyLen==0){
+				allergyTable+="<tr><td> No Allergies Found</td></tr>";
+			  }
+		  }
+		  
 
           var p = defaultPatient();
           p.birthdate = patient.birthDate;
@@ -71,7 +80,9 @@
 
           p.hdl = getQuantityValueAndUnit(hdl[0]);
           p.ldl = getQuantityValueAndUnit(ldl[0]);
-
+		
+		 p.allergies=allergyTable
+		
           ret.resolve(p);
         });
       } else {
